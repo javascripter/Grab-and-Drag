050fdd818de26f2c1e81fcc53f1de81e88d1b653
javascripter <javascripter_@livedoor.com>
2009-12-31 21:25:10 +0900

    first commit

---
+++ grab_and_drag.js
@@ -0,0 +1,57 @@
+var data;
+var orig = {
+  mousedown: function () {},
+  mousemove: function () {},
+  mouseup: function () {}
+};
+
+var waiting = Object.create(orig);
+waiting.name = "waiting";
+waiting.mousedown = function (e) {
+  data = e;
+  trans(judging);
+};
+
+var selecting = Object.create(orig);
+selecting.name = "selecting";
+selecting.mouseup = function (e) {
+  trans(waiting);
+};
+
+var judging = Object.create(orig);
+judging.mousemove = function (e) {
+  if ((e.timeStamp - data.timeStamp) > 300) {
+    trans(selecting);
+  } else {
+    trans(grabbing);
+  }
+};
+
+var grabbing = Object.create(orig);
+grabbing.name = "grabbing";
+grabbing.mousemove = function (e) {
+  window.scrollBy(data.clientX - e.clientX, data.clientY - e.clientY);
+  data = e;
+};
+grabbing.mouseup = function () {
+  trans(waiting);
+};
+
+var state;
+
+var trans = function (next) {
+  state = next;
+  document.documentElement.setAttribute("data-grab_and_drag", state.name);
+};
+
+trans(waiting);
+
+var handle = function (type) {
+  document.documentElement.addEventListener(type ,function (e) {
+    state[type](e);
+  }, false);
+};
+
+handle("mousedown");
+handle("mousemove");
+handle("mouseup");
